189500e6e825bfe4d79dfe00134270c70d77c43d
xiaoxiOk11 <xiaoxi112233ok@gmail.com>
2026-04-01 17:00:48 +0800

    update

--- pages/index.vue
+++ pages/index.vue
@@ -41,7 +41,7 @@ const showLoadingText = computed(() => {
       <div class="bootShell">
         <div class="bootBadge">{{ $t('theme.brandBadge') }}</div>
         <div class="bootLogo">
-          <img src="/ico.png" alt="QSoe Pro" class="bootIcon" decoding="async" width="72" height="72">
+          <img src="/ico.png" alt="HQBW " class="bootIcon" decoding="async" width="72" height="72">
         </div>
         <div class="bootTitle">{{ showLoadingText || $t('theme.appName') }}</div>
         <div class="bootSub">{{ $t('comm.c66') }}</div>
